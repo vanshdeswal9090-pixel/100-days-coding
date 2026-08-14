ab57648933e001060052995d2a0b148251d0c0f2
Vansh <vanshdeswal9090@gmail.com>
2026-08-14 23:57:07 +0530

    find and display the sum of the first n natural numbers

---
+++ Q8.c
@@ -0,0 +1,12 @@
+//Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("Sum of the first %d natural numbers is: %d\n", n, sum);
+    return 0;
+}
