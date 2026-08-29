0951edf04bf851af50e6f017108146a3b12fdc16
Vansh <vanshdeswal9090@gmail.com>
2026-08-29 20:00:23 +0530

    program to find the LCM of two numbers

---
+++ Q37.c
@@ -0,0 +1,39 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    printf("%d\n", lcm(a, b));
+    
+    return 0;
+}
