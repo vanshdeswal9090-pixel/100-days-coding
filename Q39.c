fe3d883a2863f07d7bfd22eac3bc18d2184907f9
Vansh <vanshdeswal9090@gmail.com>
2026-08-29 20:05:17 +0530

    find the product of odd digits of a number

---
+++ Q39.c
@@ -0,0 +1,33 @@
+//Q39: Write a program to find the product of odd digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+12345
+Output 1:
+15 (1*3*5)
+
+Input 2:
+2468
+Output 2:
+1 (no odd digits, assume 1)
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, product = 1, digit;
+    scanf("%d", &n);
+    
+    while (n != 0) {
+        digit = n % 10;
+        if (digit % 2 != 0) {
+            product *= digit;
+        }
+        n /= 10;
+    }
+    
+    printf("%d\n", product);
+    
+    return 0;
+}
