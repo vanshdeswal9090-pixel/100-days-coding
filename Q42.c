607f9308842ad69347144c1088e0e3639fc6a126
Vansh <vanshdeswal9090@gmail.com>
2026-09-01 21:47:16 +0530

    check if a number is a perfect number

---
+++ Q42.c
@@ -0,0 +1,35 @@
+//Q42: Write a program to check if a number is a perfect number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+Perfect number
+
+Input 2:
+10
+Output 2:
+Not perfect number
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == n)
+        printf("Perfect number");
+    else
+        printf("Not perfect number");
+
+    return 0;
+}
