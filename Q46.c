e68cc980be6c09c54e0c828f9e31763ed09ba0ce
Vansh <vanshdeswal9090@gmail.com>
2026-09-01 21:59:43 +0530

    print ******

---
+++ Q46.c
@@ -0,0 +1,19 @@
+// Q46: Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("*****\n");
+    }
+
+    return 0;
+}
+
